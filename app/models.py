e2a37d47665e88040563388c7728075fdfda6601
Ron Lunde <rlunde@gmail.com>
2015-12-31 06:32:14 -0800

    half step closer to oauth

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from app import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    social_id = db.Column(db.String(64), nullable=False, unique=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     stories = db.relationship('Story', backref='author', lazy='dynamic')

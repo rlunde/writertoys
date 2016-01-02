ff2c492e3f2a735408d619ef867ae59f4a3d03e8
Ron Lunde <rlunde@gmail.com>
2016-01-01 16:47:36 -0800

    I think I need to test oauth on my real domain now

--- app/views.py
+++ app/views.py
@@ -5,17 +5,21 @@ from datetime import datetime
 from app import app, db, lm, oid
 from .forms import LoginForm, EditForm
 from .models import User
+from oauth import OAuthSignIn
 
 
 @lm.user_loader
 def load_user(id):
+    print "load_user called with id %d" % id
     return User.query.get(int(id))
 
 @app.route('/')
 @app.route('/index')
-@login_required
 def index():
-    user = g.user
+    user = current_user
+    print "user is %s" % repr(user)
+    print "user.is_anonymous() is %s" % user.is_anonymous()
+    print "user.is_authenticated() is %s" % user.is_authenticated()
     stories = [
         {
             'author': {'nickname': 'John'},
@@ -28,22 +32,8 @@ def index():
     ]
     return render_template('index.html',
                            title='Home',
-                           user=user,
+                           user=current_user,
                            stories=stories)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if not hasattr(g, 'user'):
-        g.user = None
-    if g.user is not None and g.user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form)
 
 @app.route('/logout')
 def logout():
@@ -66,22 +56,6 @@ def user(nickname):
                            user=user,
                            stories=stories)
 
-
-@app.route('/edit', methods=['GET', 'POST'])
-@login_required
-def edit():
-    form = EditForm()
-    if form.validate_on_submit():
-        g.user.nickname = form.nickname.data
-        g.user.about_me = form.about_me.data
-        db.session.add(g.user)
-        db.session.commit()
-        flash('Your changes have been saved.')
-        return redirect(url_for('edit'))
-    else:
-        form.nickname.data = g.user.nickname
-        form.about_me.data = g.user.about_me
-    return render_template('edit.html', form=form)
 
 @app.route('/authorize/<provider>')
 def oauth_authorize(provider):

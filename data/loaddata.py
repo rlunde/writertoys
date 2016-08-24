59c7153aa1dd1e8eb2b08aafff71efa95a1821b6
Ron Lunde <rlunde@gmail.com>
2016-08-23 21:59:31 -0400

    updated schema to have a single denormalized names table

--- data/loaddata.py
+++ data/loaddata.py
@@ -10,122 +10,34 @@ import os
 import sys
 import getopt
 from sqlalchemy import *
-from pprint import pprint
-# for debugging, try
-# pprint(someVar)
 database = {}
 
-
-# create the nametype if it doesn't exist, and return the ID
-# this will only be called a handful of times, so it doesn't need to be
-# very efficient
-def getNameType(nametype):
-    conn = database['conn']
-    m = database['metadata']
-    engine = database['engine']
-    nameTypesTable = Table('nametypes', m, autoload=True, autoload_with=engine)
-    s = select([nameTypesTable.c.id]).where(nameTypesTable.c.type == nametype)
-    rows = conn.execute(s)
-    id = None
-    if rows.rowcount == 0:
-        ins = nameTypesTable.insert()
-        rows = conn.execute(ins, type=nametype)
-        id = rows.inserted_primary_key[0]
-    else:
-        row = rows.fetchone()
-        id = row['id']
-    return id
-
-# create the namegender if it doesn't exist, and return the ID
-# this will only be called a handful of times, so it doesn't need to be
-# very efficient
-
-
-def getNameGender(namegender):
-    conn = database['conn']
-    m = database['metadata']
-    engine = database['engine']
-    nameGendersTable = Table(
-        'namegenders', m, autoload=True, autoload_with=engine)
-    s = select([nameGendersTable.c.id]).where(
-        nameGendersTable.c.type == namegender)
-    rows = conn.execute(s)
-    id = None
-    if rows.rowcount == 0:
-        ins = nameGendersTable.insert()
-        rows = conn.execute(ins, type=namegender)
-        id = rows.inserted_primary_key[0]
-    else:
-        row = rows.fetchone()
-        id = row['id']
-    return id
-
-# create the nameorigin if it doesn't exist, and return the ID
-# this will only be called a handful of times, so it doesn't need to be
-# very efficient
-
-
-def getNameOrigin(nameorigin):
-    conn = database['conn']
-    m = database['metadata']
-    engine = database['engine']
-    nameOriginsTable = Table(
-        'nameorigins', m, autoload=True, autoload_with=engine)
-    s = select([nameOriginsTable.c.id]).where(
-        nameOriginsTable.c.name == nameorigin)
-    rows = conn.execute(s)
-    id = None
-    if rows.rowcount == 0:
-        ins = nameOriginsTable.insert()
-        rows = conn.execute(ins, name=nameorigin)
-        id = rows.inserted_primary_key[0]
-    else:
-        row = rows.fetchone()
-        id = row['id']
-    return id
-
-
-def getRawNameId(rawname):
-    conn = database['conn']
-    m = database['metadata']
-    engine = database['engine']
-    rawNamesTable = Table('rawnames', m, autoload=True, autoload_with=engine)
-    s = select([rawNamesTable.c.id]).where(rawNamesTable.c.name == rawname)
-    rows = conn.execute(s)
-    id = None
-    if rows.rowcount == 0:
-        ins = rawNamesTable.insert()
-        rows = conn.execute(ins, name=rawname)
-        id = rows.inserted_primary_key[0]
-    else:
-        row = rows.fetchone()
-        id = row['id']
-    return id
-
-# this is not very efficient, since we should do an insert if not exists in SQLAlchemy/Postgres,
-# rather than look for it here, and insert if not found. Still, we'll do it this way, since
-# this is a one-time load rather than an API handler.
-
-
+'''
+Load a file of the specified gender (female,male,any) and type (first,last).
+Hardcode the origin to 'any' for now.
+'''
 def loadlist(filename, namegender, nametype):
     conn = database['conn']
     m = database['metadata']
     engine = database['engine']
 
-    # import pdb; pdb.set_trace()
+    #import pdb; pdb.set_trace()
     namesTable = Table('names', m, autoload=True, autoload_with=engine)
     # note: the name data has already been extracted into just a list; it's
     # not a csv
     with open(filename, 'rb') as listfile:
         for rawname in listfile:
             rawname = rawname.strip()
-            s = select([namesTable.c.rawnames_id]).where(and_(
-                    "namesTable.c.name" == rawname,
-                    "namesTable.c.type" == nametype,
-                    "namesTable.c.gender" == namegender,
-                    "namesTable.c.origin" == 'any'
+            # the first time this is run, there should be no collisions, so
+            # it might be more efficient to just try to insert it and catch errors
+            s = select([namesTable]).where(and_(
+                (namesTable.c.name == rawname),
+                (namesTable.c.type == nametype),
+                (namesTable.c.gender == namegender),
+                (namesTable.c.origin == 'any')
                 )
             )
+            #print s
             rows = conn.execute(s)
             id = None
             if rows.rowcount == 0:
--- data/schema.ddl
+++ data/schema.ddl
@@ -1,44 +1,16 @@
 -- for database "writertoys"
 
 DROP TABLE IF EXISTS Names CASCADE;
-DROP TABLE IF EXISTS RawNames CASCADE;
-DROP TABLE IF EXISTS NameTypes CASCADE;
-DROP TABLE IF EXISTS NameGenders CASCADE;
-DROP TABLE IF EXISTS NameOrigins CASCADE;
 DROP TABLE IF EXISTS Edges CASCADE;
 DROP TABLE IF EXISTS Nodes CASCADE;
-
--- just a name (first or last, male or female or either, ...)
-CREATE TABLE RawNames (
-    id serial primary key,
-    name TEXT
-);
-
--- first or last
-CREATE TABLE NameTypes (
-    id serial primary key,
-    type TEXT
-);
-
--- male, female, or either
-CREATE TABLE NameGenders (
-    id serial primary key,
-    type TEXT
-);
-
--- American, Asian, Scandinavian, ...
-CREATE TABLE NameOrigins (
-    id serial primary key,
-    name TEXT
-);
 
 -- a name with type, origin, and gender
 CREATE TABLE Names (
     id serial primary key,
-    RawNames_id integer REFERENCES RawNames,
-    NameTypes_id integer REFERENCES NameTypes,
-    NameGenders_id integer REFERENCES NameGenders,
-    NameOrigins_id integer REFERENCES NameOrigins
+    name TEXT,
+    type TEXT, -- e.g. first, last
+    gender TEXT, -- e.g. male, female, any
+    origin TEXT -- e.g. country
 );
 
 CREATE TABLE Nodes (

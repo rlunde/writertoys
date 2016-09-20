db6eddeccf7e139ff945836c1adf9041073d337c
Ron Lunde <ron.lunde@iovation.com>
2016-09-19 23:45:08 -0700

    adding some controls in names form

--- client/js/writertoys.jsx
+++ client/js/writertoys.jsx
@@ -2,7 +2,15 @@ var NamesList = React.createClass({
     render: function() {
         return (
             <div className="namesList">
-                This is a NamesList div.
+              <span>Number of names: </span>
+                <input type="number" id="numNames"></input>
+                <span>Type of names: </span>
+                <select className="nameGenders" id="nameGender">
+                    <option value="female">female</option>
+                    <option value="male">male</option>
+                    <option value="any">any</option>
+                </select>
+                <button className="namesButton">Get Names!</button>
             </div>
         );
     }

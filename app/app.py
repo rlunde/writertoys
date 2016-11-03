8d07114b751ee0fc0cb1341ea64b4dc8314ad1d1
Ron Lunde <rlunde@gmail.com>
2016-11-02 23:09:01 -0400

    getting ready to update layout

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'cli'))
-import randnames
+from cli import randnames
 
 app = Flask(__name__)
 

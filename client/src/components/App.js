e03cfa1b00416d36fc19a9492c6a92af3ba40959
Ron Lunde <rlunde@gmail.com>
2018-01-02 22:41:47 -0500

    fix eslint errors

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,12 +8,12 @@ const navbarInstance = (
   <Navbar inverse>
     <Navbar.Header>
       <Navbar.Brand>
-        <a href="#">WriterToys</a>
+        <a href="/">WriterToys</a>
       </Navbar.Brand>
     </Navbar.Header>
     <Nav>
-      <NavItem eventKey={1} href="#">Link</NavItem>
-      <NavItem eventKey={2} href="#">Link</NavItem>
+      <NavItem eventKey={1} href="/">Link</NavItem>
+      <NavItem eventKey={2} href="/">Link</NavItem>
       <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
         <MenuItem eventKey={3.1}>Action</MenuItem>
         <MenuItem eventKey={3.2}>Another action</MenuItem>

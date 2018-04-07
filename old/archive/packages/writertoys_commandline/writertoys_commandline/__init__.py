dac50684bd7e0f6a12d2fe424424cae659949880
Ron Lunde <rlunde@gmail.com>
2018-04-07 14:46:28 -0400

    remove old stuff

--- old/archive/packages/writertoys_commandline/writertoys_commandline/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Ron Lunde'
-__email__ = 'r_lunde@yahoo.com'
-__version__ = '0.1.0'
--- old/archive/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#source ./venv/bin/activate
-source ./source_me
-./run.py
